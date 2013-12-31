80814995764acbad52c28e352d0a5665966e3a71
andyzg <zhangandy94@gmail.com>
2013-12-31 00:44:21 -0500

    Readd Bubble Sort

---
+++ Java/src/BubbleSort.java
@@ -0,0 +1,49 @@
+/**
+ * Implementation of Bubble Sort
+ * @author Andy
+ *
+ */
+public class BubbleSort {
+
+	
+	public static void main(String args[])
+	{
+		int list[] = {3,1,9,7,5,2,10};
+		
+		// Before sort
+		printArray(list);
+		
+		bubbleSort(list);
+		
+		// After sort
+		printArray(list);
+	}
+		
+	private static void printArray(int []list)
+	{
+		for (int i=0; i<list.length; i++)
+		{
+			System.out.print(list[i] + "\t");
+		}
+		System.out.println();
+		return;
+	}
+
+	private static void bubbleSort(int[] list)
+	{
+		int temp;
+		
+		for (int i=0; i<list.length-1 ; i++)
+		{
+			for (int j=0; j<list.length-1-i; j++)
+			{
+				if (list[j] > list[j+1])
+				{
+					temp = list[j];
+					list[j] = list[j+1];
+					list[j+1] = temp;
+				}
+			}
+		}
+	}
+}
