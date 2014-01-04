a73cfec5ff12cf27ec5ec257ec83512b917518d0
andyzg <zhangandy94@gmail.com>
2014-01-03 21:29:10 -0500

    Implemented 1_4

--- Java/src/stringAndArrayProblems/P1_4.java
+++ Java/src/stringAndArrayProblems/P1_4.java
@@ -1,6 +1,7 @@
 package stringAndArrayProblems;
 
 import java.util.Hashtable;
+import java.util.Set;
 
 public class P1_4 {
 
@@ -15,8 +16,45 @@ public class P1_4 {
 		{
 			if (ht.containsKey(a.charAt(i)))
 			{
-				
+				ht.put(a.charAt(i),ht.get(a.charAt(i))+1);
+			}
+			else
+			{
+				ht.put(a.charAt(i), 1);
 			}
 		}
+		
+		for (int i=0; i<b.length(); i++)
+		{
+			if (!ht.containsKey(b.charAt(i)))
+				return false;
+			else
+				ht.put(b.charAt(i), ht.get(b.charAt(i))-1);
+		}
+		
+		Set<Character> keys = ht.keySet();
+		
+		for (char x:keys)
+		{
+			if (ht.get(x) != 0)
+			{
+				System.out.println(x);
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args)
+	{
+		String a = "Andy Zhang";
+		String b = "Kevin Li";
+		String c = "ndAy ngZha";
+		String d = "andandand";
+		String e = "ndandanda";
+		
+		System.out.println(run(a,b));
+		System.out.println(run(c,a));
+		System.out.println(run(d,e));
 	}
 }
